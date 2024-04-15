5eff34b56eb58ac7beebcc7253b68dd4e41b2038
Mich CNC <michochieng@gmail.com>
2024-04-15 15:44:12 -0700

    Created page to display image placeholders

---
+++ app/img-placeholders/page.tsx
@@ -0,0 +1,10 @@
+import DefaultBlur from "@/components/Placeholders/DefaultBlur";
+
+export default function Placeholders() {
+
+    return (
+      <main className="flex flex-col items-center justify-start w-full gap-[5rem]">
+        <DefaultBlur/>
+      </main>
+    );
+}
