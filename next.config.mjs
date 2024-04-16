c11f9e80554a6d8c0fb223cda7683d6435600738
Mich CNC <michochieng@gmail.com>
2024-04-16 15:24:57 -0700

    Fixed unsplash whitelist

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,21 @@ import withPlaiceholder from "@plaiceholder/next";
 /*
     Plaiceholder docs: https://plaiceholder.co/docs
 */
- 
+
 /**
  * @type {import('next').NextConfig}
  */
 const config = {
-    images: {
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'images.unsplash.com',
-            },
-          ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
- 
+
 export default withPlaiceholder(config);
