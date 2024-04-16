e5e614d7a03b1f8aec53530d270eb299e99e99bf
Mich CNC <michochieng@gmail.com>
2024-04-16 10:15:10 -0700

    Converted to mjs to support Plaiceholder package

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import withPlaiceholder from "@plaiceholder/next";
+/*
+    Plaiceholder docs: https://plaiceholder.co/docs
+*/
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = {
+  // your Next.js config
+};
+ 
+export default withPlaiceholder(config);
