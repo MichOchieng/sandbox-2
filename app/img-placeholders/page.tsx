c3efcb4387a6b9ff2a67c78baa973b62149fdacc
Mich CNC <michochieng@gmail.com>
2024-04-16 10:46:58 -0700

    Added new image test

--- app/img-placeholders/page.tsx
+++ app/img-placeholders/page.tsx
@@ -1,10 +1,14 @@
+import ColourPlaceholder from "@/components/Placeholders/ColourPlaceholder";
+import CustomBlur from "@/components/Placeholders/CustomBlur";
 import DefaultBlur from "@/components/Placeholders/DefaultBlur";
 
 export default function Placeholders() {
 
     return (
-      <main className="flex flex-col items-center justify-start w-full gap-[5rem]">
+      <main className="flex flex-col items-center justify-start w-full gap-[5rem] py-[5rem]">
         <DefaultBlur/>
+        <CustomBlur/>
+        <ColourPlaceholder/>
       </main>
     );
 }
