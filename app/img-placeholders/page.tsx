0d58a12d7f2947e429cd7808d589bf5e5f3c676d
Mich CNC <michochieng@gmail.com>
2024-04-16 15:22:51 -0700

    Added more examples

--- app/img-placeholders/page.tsx
+++ app/img-placeholders/page.tsx
@@ -1,6 +1,10 @@
 import ColourPlaceholder from "@/components/Placeholders/ColourPlaceholder";
 import CustomBlur from "@/components/Placeholders/CustomBlur";
 import DefaultBlur from "@/components/Placeholders/DefaultBlur";
+import DynamicBlur from "@/components/Placeholders/DynamicBlur";
+import DynamicColourPlaceholder from "@/components/Placeholders/DynamicColourPlaceholder";
+import FadeInPlaceholder from "@/components/Placeholders/FadeInPlaceholder";
+import SlidePlaceholder from "@/components/Placeholders/SlidePlaceholder";
 
 export default function Placeholders() {
 
@@ -9,6 +13,10 @@ export default function Placeholders() {
         <DefaultBlur/>
         <CustomBlur/>
         <ColourPlaceholder/>
+        <DynamicBlur/>
+        <DynamicColourPlaceholder/>
+        <FadeInPlaceholder/>
+        <SlidePlaceholder/>
       </main>
     );
 }
