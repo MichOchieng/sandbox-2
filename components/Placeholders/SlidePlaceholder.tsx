b0caa34846366737f13f67d39bbb350ee1f4f006
Mich CNC <michochieng@gmail.com>
2024-04-16 15:25:15 -0700

    First commit

---
+++ components/Placeholders/SlidePlaceholder.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from "next/image";
+
+import { getPlaiceholder } from "plaiceholder";
+
+const SlidePlaceholder = () => {
+  return (
+    <div>SlidePlaceholder</div>
+  )
+}
+
+export default SlidePlaceholder
